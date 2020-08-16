853d86d88937c9f4dcc387560fd4db3a4ab29f08
Alex2055 <alexfromnashville@gmail.com>
2020-08-16 15:32:47 -0500

    post, get, delet requests

---
+++ Develop/server.js
@@ -0,0 +1,83 @@
+const fs = require('fs');
+const express = require('express');
+const app = express()
+const PORT = 3001;
+const path = require('path')
+const { v4: uuidv4 } = require('uuid');
+const bodyParser = require('body-parser')
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+
+})
+
+
+
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', function (err, data) {
+    if (err) {
+      res.json([]);
+    }
+    else {
+      res.json(JSON.parse(data));
+    }
+  })
+
+});
+
+app.post('/api/notes', (req, res) => {
+
+  const note = req.body;
+  note.id = uuidv4();
+  fs.readFile('./db/db.json', function (err, data) {
+    let notes = [];
+    if (!err && data) {
+      notes = JSON.parse(data);
+    }
+    notes.push(note);
+
+    fs.writeFile('./db/db.json', JSON.stringify(notes), err => {
+      if (err) {
+        res.status(500).send('error');
+      }
+      else {
+        res.json(note)
+      }
+    })
+  })
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  
+  fs.readFile('./db/db.json', (err, data) => {
+    let notes = [];
+    if (!err && data) {
+      notes = JSON.parse(data);
+    }
+    notes = notes.filter(note => note.id !== id);
+    fs.writeFile('./db/db.json', JSON.stringify(notes), err => {
+      if(err){
+        res.status(500).send('error');
+      }
+      else{
+        res.sendStatus(200);
+      }
+    })
+
+  });
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
